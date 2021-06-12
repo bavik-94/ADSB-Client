6dd555b3af78d0076120b6cd78945403cd9df963
Philip Cara <philipcara@gmail.com>
2021-06-12 08:36:59 -0400

    moved functions to aircraft

--- scripts/aircraft-handler.js
+++ scripts/aircraft-handler.js
@@ -1,68 +1,51 @@
-let active = []
+let active = [];
 
-function stateHandler(object){ //updates existing aircraft or creates a new one if a match is not found
-    let match = false
-    for (i=0; i < object.length - 1; i ++){
-        for (j=0; j < active.length; j++){
-            if (active[j].hex_ident == object[i].hex_ident){ 
-                active[j].update(object[i])
-                active[j].setMarker();
-                match = true
-                break;
-            }
-        }
+function stateHandler(stateMessage){ //updates existing aircraft or creates a new one if a match is not found
     
-    if (match == false){
-        let newAircraft = new Aircraft(object[i])
-        newAircraft.setMarker();
-        active.push(newAircraft)
+    for (var i=0; i < stateMessage.length ; i ++){
+        var aircraft = getAircraft(stateMessage[i].hex_ident);
+        
+        if (aircraft){ //existing
+            aircraft.update(stateMessage[i]);
+            aircraft.setMarker();
+            
+        }else{
+            var newAircraft = new Aircraft(stateMessage[i]); // new
+            newAircraft.setMarker();
+            active.push(newAircraft);
+        }    
     }
-    }
-cleanUpExpired()
-logStuff()
+    cleanUpExpired();
 }
 
-function cleanUpExpired(){ // remove aircraft that are expired
-    var now = new Date() 
-    var epoch = Math.round(now.getTime() / 1000)
+function cleanUpExpired(){ // remove aircraft that haven't been updated in 60 seconds or longer
+    var now = new Date();
+    var epoch = Math.round(now.getTime() / 1000);
     for (var i = active.length - 1; i >= 0; i--){
         if (epoch - active[i].live >= 60){
-            aircraft = getSelectedAircraft
-            if (aircraft == active[i]){
-                hideInfoBox()
-                deselectFlight()
+            if (active[i].selected == true){
+                active[i].removeInspected();
             }
-            active[i].removeMarker()
-            active.splice(i,1)
+            active[i].removeMarker();
+            active.splice(i,1);
         }
     }
 }
 
 function getAircraft(hex_ident){
-    for (i = 0; i < active.length;i++){
+    for (var i = 0; i < active.length;i++){
         if (active[i].hex_ident == hex_ident){
             return active[i]
         }
     }
-return null
+    return null;
 }
 
 function getSelectedAircraft(){
-    for (i = 0; i < active.length;i++){
-        if (active[i].selected == true){
-            return active[i]
+    for (var i = 0; i < active.length;i++){
+        if (active[i].selected){
+            return active[i];
         }
     }
-return null
-}
-
-function logStuff(){
-    aSel = []
-    for (i = 0; i < active.length; i++){
-        if (active[i].history.length > 0){
-            aSel.push(active[i].hex_ident + " : " + active[i].history.length + ' : ' + active[i].selected)
-        }
-    }
-    console.log('------------------')
-    console.log(aSel)
+    return null;
 }
